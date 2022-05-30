b453a5949d2e23851ea809572d3e48dd204adab9
santiagoaluas <santiagoalulema@gmail.com>
2022-05-30 01:25:42 -0500

    casi terminado

---
+++ src/assets/markets.js
@@ -0,0 +1,15 @@
+const markets = [
+    { 
+        title:"RESPONSABILIDAD",
+        descriptionImage:"./img/headerGenerosidad.png",
+        patt: "./patts/generosidad.patt",
+        image: "./img/generosidad.png"
+    },
+
+    { 
+        title:"RESPONSABILIDAD",
+        descriptionImage:"./img/headerAmistad.png",
+        patt: "./patts/amistad.patt",
+        image: "./img/amistad.png"
+    }
+]
