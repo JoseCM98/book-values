35cdc9cc02dc013a5cf93062c6784ff2f6950ff3
JoseCM98 <87342566+JoseCM98@users.noreply.github.com>
2022-05-30 15:32:31 -0500

    patt respeto

--- src/assets/markets.js
+++ src/assets/markets.js
@@ -1,15 +1,21 @@
 const markets = [
-    { 
+    {
         title:"RESPONSABILIDAD",
         descriptionImage:"./img/headerGenerosidad.png",
         patt: "./patts/generosidad.patt",
         image: "./img/generosidad.png"
     },
 
-    { 
+    {
         title:"RESPONSABILIDAD",
         descriptionImage:"./img/headerAmistad.png",
         patt: "./patts/amistad.patt",
         image: "./img/amistad.png"
-    }
+    },
+    {
+      title:"RESPETO",
+      descriptionImage:"./img/headerRespeto.png",
+      patt: "./patts/respeto.patt",
+      image: "./img/respeto.png"
+    },
 ]
